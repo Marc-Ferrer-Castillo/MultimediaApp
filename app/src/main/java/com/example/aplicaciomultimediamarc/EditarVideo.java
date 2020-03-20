18a7640283c2a969a4b84a81605eb837f7c8d630
Marc <Marc-Ferrer-Castillo@users.noreply.github.com>
2020-03-20 22:00:19 +0100

    Commit

--- app/src/main/java/com/example/aplicaciomultimediamarc/EditarVideo.java
+++ app/src/main/java/com/example/aplicaciomultimediamarc/EditarVideo.java
@@ -1,70 +1,242 @@
 package com.example.aplicaciomultimediamarc;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.provider.Settings;
-import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import com.deep.videotrimmer.DeepVideoTrimmer;
-import com.deep.videotrimmer.interfaces.OnTrimVideoListener;
+import android.widget.VideoView;
 
-public class EditarVideo extends AppCompatActivity implements OnTrimVideoListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class EditarVideo extends AppCompatActivity {
+
+    public static final int REQUEST_PERMISSIONS_CODE = 2909;
+    public static final int RECORTE_MINIMO = 1000;
+    private VideoView videoView;
+    private CountDownTimer countDownTimer;
+    private long tiempoExtra = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_video);
 
+         videoView = findViewById(R.id.videoeditor);
+
+        // Permisos
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.System.canWrite(this)) {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE}, 2909);
-            } else {
-                // continue with your code
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS_CODE);
             }
-        } else {
-            // continue with your code
+            else {
+                editarVideo(videoView, getIntent().getData() );
+            }
+        }
+        else {
+            editarVideo(videoView, getIntent().getData() );
+        }
+    }
+
+    // Editor de video
+    @SuppressLint("ClickableViewAccessibility")
+    private void editarVideo(final VideoView videoView, Uri uri) {
+        // El videoView usa la Uri recibida por parametro
+        videoView.setVideoURI( uri );
+
+        // Botón de play
+        final ImageView playBoton = findViewById(R.id.playboton);
+
+        // Contadores
+        final TextView contadorInicio = findViewById(R.id.tiempoinicio);
+        final TextView tiempo         = findViewById(R.id.tiempoactual);
+        final TextView contadorFin    = findViewById(R.id.tiempofin);
+
+        // Seekbars
+        final SeekBar controlIzquierdo = findViewById(R.id.marcaizquierda);
+        final SeekBar controlDerecho   = findViewById(R.id.marcaderecha);
+
+        // Obtiene la duración del video
+        final int duracion = extraerDuracion(uri);
+
+        // La duración máxima será la del video
+        controlIzquierdo.setMax(duracion);
+        controlDerecho.setMax(duracion);
+
+        // Tiempo del contador derecho inicial
+        contadorFin.setText(convertirTiempo(duracion));
+
+        // Temporizador
+        countDownTimer = new CountDownTimer(duracion, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tiempo.setText( convertirTiempo(videoView.getCurrentPosition()) );
+
+                // Si el tiempo del video coincide con la posición del control derecho se terminará el tiempo
+                if (convertirTiempo(videoView.getCurrentPosition()).equals(convertirTiempo(controlDerecho.getProgress()))){
+                    videoView.pause();
+                    playBoton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        };
+
+        // El control derecho empezará en la posición equivalente a la duración del video
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            controlDerecho.setProgress(duracion, true);
         }
 
+        // Al hacer click sobre el video
+        videoView.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent)
+            {
 
-        DeepVideoTrimmer videoTrimmer = findViewById(R.id.videorecorte);
-        videoTrimmer.setMaxDuration(500);
-        videoTrimmer.setMaxFileSize(30);
-        videoTrimmer.setOnTrimVideoListener(this);
-        videoTrimmer.setVideoURI(getIntent().getData());
+                if (videoView.isPlaying())
+                {
+                    playBoton.setVisibility(View.VISIBLE);
+                    videoView.pause();
+                    countDownTimer.cancel();
+                }
+                else
+                {
+                    playBoton.setVisibility(View.INVISIBLE);
+                    videoView.seekTo(controlIzquierdo.getProgress());
+                    videoView.start();
+                    countDownTimer.start();
+                }
+                return false;
+            }
+        });
 
+        // Al finalizar el video
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                countDownTimer.cancel();
+            }
+        });
+
+        // Al deslizar el control izquierdo
+        controlIzquierdo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress <= controlDerecho.getProgress() - RECORTE_MINIMO){
+                    videoView.seekTo(progress);
+                    tiempo.setText(convertirTiempo(progress));
+                    contadorInicio.setText(convertirTiempo(progress));
+                }
+                // La posición del control izquierdo no podrá ser mayor a la del derecho
+                else{
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        controlIzquierdo.setProgress(controlDerecho.getProgress() - RECORTE_MINIMO, true);
+                    }
+                    videoView.seekTo(controlDerecho.getProgress() - RECORTE_MINIMO);
+                    tiempo.setText(convertirTiempo(controlDerecho.getProgress() - RECORTE_MINIMO));
+                    contadorInicio.setText(convertirTiempo(controlDerecho.getProgress() - RECORTE_MINIMO));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                tiempoExtra = seekBar.getProgress();
+                playBoton.setVisibility(View.INVISIBLE);
+                videoView.start();
+                countDownTimer.start();
+            }
+        });
+
+        // Al deslizar el control derecho
+        controlDerecho.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Si el progreso del control es superior al del control izquierdo
+                if (progress >= controlIzquierdo.getProgress() + RECORTE_MINIMO){
+                    contadorFin.setText(convertirTiempo(progress));
+                    tiempo.setText(convertirTiempo(progress));
+                    videoView.seekTo(progress);
+                }
+                else{
+                    // La posición del control derecho no podrá ser menor a la del izquierdo
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        controlDerecho.setProgress(controlIzquierdo.getProgress() + RECORTE_MINIMO, true);
+                    }
+                    contadorFin.setText(convertirTiempo(controlIzquierdo.getProgress() + RECORTE_MINIMO));
+                    tiempo.setText(convertirTiempo(controlIzquierdo.getProgress() + RECORTE_MINIMO));
+                    videoView.seekTo(controlIzquierdo.getProgress() + RECORTE_MINIMO);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                videoView.seekTo(controlIzquierdo.getProgress());
+                videoView.pause();
+            }
+        });
+
+    }
+
+    // Convierte los milisegundos recibidos por parametro en un string con minutos y segundos
+    @SuppressLint("DefaultLocale")
+    private String convertirTiempo (long milisegundos){
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(milisegundos),
+                TimeUnit.MILLISECONDS.toSeconds(milisegundos) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milisegundos))
+        );
+    }
+
+    // Extrae la duración del video
+    private int extraerDuracion(Uri uri) {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(EditarVideo.this, uri);
+
+        String tiempo = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        int duracion = Integer.parseInt(tiempo);
+
+
+        retriever.release();
+
+        return duracion;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 2909: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("Permission", "Granted");
-                } else {
-                    Log.e("Permission", "Denied");
-                }
-                return;
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            // permisos concedidos
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                editarVideo(videoView, getIntent().getData() );
+            }
+            else {
+                Toast.makeText(EditarVideo.this, "Permissos denegats", Toast.LENGTH_LONG).show();
             }
         }
-    }
-    @Override
-    public void getResult(Uri uri) {
-        savefile(uri);
-    }
-
-    @Override
-    public void cancelAction() {
-
-    }
-
-    void savefile(Uri sourceuri)
-    {
-        Toast.makeText(EditarVideo.this, "Video guardat correctament", Toast.LENGTH_LONG).show();
-
     }
 }
